cfdfec0b71bf2430a3a49c45f6c65562a143edb2
Misha Kotwica <udiknedormin@gmail.com>
2020-06-21 04:57:03 +0200

    rewrite old var -> old let in procs
    cleaner generated code

--- contracts/contexts/handlers/conProc.nim
+++ contracts/contexts/handlers/conProc.nim
@@ -7,7 +7,14 @@ proc proceduralContract(thisNode: NimNode): NimNode =
   ## Handles contracts for procedures and converters.
   contextHandle(thisNode, @ContractKeywordsProc) do (it: Context):
     if it.olds != nil:
-      let li = newStmtList()
-      li.add it.olds
-      li.add updateOldValues(it.olds)
-      it.olds = li
+      if it.olds.kind == nnkVarSection:
+        let new_olds = newNimNode(nnkLetSection)
+        for i, section in it.olds:
+          # `var name: type(impl)` --> `let name = impl`
+          let name = section[0]
+          let type_src = section[1][1]
+          template systemDeepCopy(n): untyped = system.deepCopy(n)
+          let impl = getAst(systemDeepCopy(type_src))
+          let new_section = newIdentDefs(name, newEmptyNode(), impl)
+          new_olds.add new_section
+        it.olds = new_olds
