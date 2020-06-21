0af40830a618150effd050ac0b9ca1c61db51dc9
Misha Kotwica <udiknedormin@gmail.com>
2020-06-21 08:38:44 +0200

    better docs for forall and forsome

--- contracts/features/quantifiers.nim
+++ contracts/features/quantifiers.nim
@@ -8,16 +8,25 @@ proc getForLoopElems(code: NimNode): (NimNode, NimNode) =
     error("`X in Y` notation expected, found: $1".format(code.repr))
   result = (code[1], code[2])
 
+proc flattenCondList(cond: NimNode): NimNode =
+  if cond.kind == nnkStmtList or cond.kind == nnkStmtListExpr:
+    var newCond = cond[0]
+    for child in cond[1 ..^ cond.len]:
+      newCond = infix(newCond, "and", child)
+    newCond
+  else:
+    cond
+
 macro forsome* (what, cond: untyped): untyped =
   ## For-like existential quantifier with natural syntax.
   ## It works similar to
-  ## `sequtils.any <http://nim-lang.org/docs/sequtils.html#any>`_
-  ## but is more generic.
+  ## `sequtils.anyIt <http://nim-lang.org/docs/sequtils.html#any>`_.
   ##
   ## .. code-block:: nim
   ##  require:
   ##    forsome x in arr: x == key
   let (first, second) = getForLoopElems(what)
+  let newCond = flattenCondList(cond)
   template forImpl(fi, sec, cond) =
     block:
       var flag = false
@@ -26,18 +35,18 @@ macro forsome* (what, cond: untyped): untyped =
           flag = true
           break
       flag
-  result = getAst(forImpl(first, second, cond))
+  result = getAst(forImpl(first, second, newCond))
 
 macro forall* (what, cond: untyped): untyped =
   ## For-like universal quantifier with natural syntax.
   ## It works similar to
-  ## `sequtils.all <http://nim-lang.org/docs/sequtils.html#all>`_
-  ## but is more generic.
+  ## `sequtils.allIt <http://nim-lang.org/docs/sequtils.html#all>`_.
   ##
   ## .. code-block:: nim
   ##  require:
   ##    forall x in arr: x > 0
   let (first, second) = getForLoopElems(what)
+  let newCond = flattenCondList(cond)
   template forImpl(fi, sec, cond) =
     block:
       var flag = true
@@ -46,4 +55,4 @@ macro forall* (what, cond: untyped): untyped =
           flag = false
           break
       flag
-  result = getAst(forImpl(first, second, cond))
+  result = getAst(forImpl(first, second, newCond))
